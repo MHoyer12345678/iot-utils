1f70cbf54ecffc1f59a4435368b127f962f1537e
MHoyer12345678 <mhoyer.oss-devel@freenet.de>
2022-03-06 20:11:28 +0100

    add network routing tool to connect host and iot device

---
+++ host-target-network-routing/route_network.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#script enables network package forwarding from wlan0 to eth0. This can be used to enable internet access on development targets
+
+#enable ip forwarding
+echo "1" > /proc/sys/net/ipv4/ip_forward
+
+#add masquerade rule to post processing in nat table
+iptables -t nat -A POSTROUTING -o wlp3s0 -j MASQUERADE
